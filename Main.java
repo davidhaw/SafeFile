1533706737977c08267794394ce84ed86d22b5f2
David <davidhaw2005@gmail.com>
2018-04-29 16:26:03 +0100

    Added in Decryption
    Added in the ability to decrypt ceaser cyphers. I am hoping to add my own cypher, which works by turning all the characters into their respective number, and then multiplying or dividing the numbers to get an encrypted number that if decrypted correctly, would make the same message.

--- Main.java
+++ Main.java
@@ -16,71 +16,73 @@ public class Main {
         System.out.println();
         
         //Check if user wants to encrypt or decrypt
-        //DECRYPT HAS NOT BEEN ADDED IN SO CODE IS COMMENTED OUT
-        //System.out.println("Would you like to Encrypt or Decrypt (Select one in the same spelling)");
-       // String EorD = scanner.next();
+        System.out.println("Would you like to Encrypt or Decrypt (Select one in the same spelling)");
+        String EorD = scanner.next();
         
-        //if (EorD.equals("Encrypt")) {
+        if (EorD.equals("Encrypt")) {
         	
-        	Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
 
-        	//Check how the user wants to encrypt
-        	System.out.println("How Would You Like to Encrypt? - Available Currently: Ceaser Cypher");
-        	System.out.println("For Ceaser Cypher, enter Ceaser"); //More would be added in as more encryption ways are added into the program
-        	
-        	String howE = scanner.next();
-        	
-        	if (howE.equals("Ceaser")) {
-        		
-                System.out.println("What is the text you would like to encrypt?");
-                String textE = scanner1.next();
-                
-                System.out.println("What is the offset for the Ceaser Cypher?");
-        		String numE = scanner1.next();
-        				
-        		int numEi = Integer.parseInt(numE);
-        	
-        		String encrypted = Encrypter.encode(textE, numEi);
-                
-        		System.out.println("Below is encrypted Text with Ceaser Cypher, offset of " + numE);
-        		System.out.println(encrypted);
-        		
-                // close the scanner
-                scanner1.close();
+    	//Check how the user wants to encrypt
+    	System.out.println("How Would You Like to Encrypt? - Available Currently: Ceaser Cypher");
+    	System.out.println("For Ceaser Cypher, enter Ceaser"); //More would be added in as more encryption ways are added into the program
+    	
+    	String howE = scanner.next();
+    	
+    	if (howE.equals("Ceaser")) {
+    		
+            System.out.println("What is the text you would like to encrypt?");
+            String textE = scanner1.next();
+            
+            System.out.println("What is the offset for the Ceaser Cypher?");
+    		String numE = scanner1.next();
+    				
+    		int numEi = Integer.parseInt(numE);
+    	
+    		String encrypted = Encrypter.encode(textE, numEi);
+            
+    		System.out.println("Below is encrypted Text with Ceaser Cypher, offset of " + numE);
+    		System.out.println(encrypted);
+    		
+            // close the scanner
+            scanner1.close();
+            
                 scanner.close();
         		
         	}
-        //}
-        	
-        } 
-        //else if (EorD.equals("Decrypt")){
+        } else if (EorD.equals("Decrypt")){
         		
+        		System.out.println("Decrypting message");
+        	
         		//Asks the user how they want to decrypt
-            	//System.out.println("How Would You Like to Decrypt? - Available Currently: Ceaser Cypher");
-            	//String howD = scanner.next();
+            	System.out.println("How Would You Like to Decrypt? - Available Currently: Ceaser Cypher");
+            	System.out.println("For Ceaser Cypher, enter Ceaser"); //More would be added in as more encryption ways are added into the program
+            	String howD = scanner.next();
             	
-            	 //if(howD.equals("Ceaser Cypher")) {
+            	 if(howD.equals("Ceaser")) {
             		
             		//Asks user the text and offset they want to use to decrypt the ceaser cypher
-            		//System.out.println("What is the text you would like to decrypt?");
-                   // String textD = scanner.next();
+            		System.out.println("What is the text you would like to decrypt?");
+                    String textD = scanner.next();
                     
-                    //System.out.println("What is the offset for the Ceaser Cypher?");
-            		//String numD = scanner.next();
+                    System.out.println("How many characters away is the original message?");
+            		String numD = scanner.next();
             				
-            		//int numDi = Integer.parseInt(numD);
+            		int numDi = Integer.parseInt(numD);
             	
-            		//String decrypted = Encrypter.encode(textD, numDi);
+            		int newNumDi = 26-numDi;
+            		
+            		String decrypted = Encrypter.encode(textD, newNumDi);
                     
-            		//System.out.println(decrypted);
+            		System.out.println(decrypted);
             		
-            		//System.out.println("please close and open the application again if you want to do another calculation");
+            		System.out.println("please close and open the application again if you want to do another calculation");
             		
             		
-            	//}
+            	}
             	
             	
-            	
-        	//}
+         }
+        	}
         	 
 }
